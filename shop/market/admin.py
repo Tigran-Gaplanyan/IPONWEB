ee85e98c2b1ad7132643b1d0a446cf133be50cfd
Tigran-Gaplanyan <tigrangaplanyan@gmail.com>
2023-02-11 17:14:58 +0400

    shop

---
+++ shop/market/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+
+class StoreCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "picture")
+    search_fields = ['id']
+
+
+class ItemCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "picture")
+    search_fields = ['id']
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("id", "avatar", "registered_at")
+    search_fields = ['id']
+
+
+class StoreOwnerAdmin(admin.ModelAdmin):
+    list_display = ("id", "avatar", "registered_at")
+    search_fields = ['id']
+
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ['id']
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "picture", "price", "quantity", "info")
+    search_fields = ['id']
+
+
+class MyBagAdmin(admin.ModelAdmin):
+    list_display = ("id", "total_price")
+    search_fields = ['id']
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ("id", "buy_time", "total_price")
+    search_fields = ['id']
+
+
+admin.site.register(StoreCategory, StoreCategoryAdmin)
+admin.site.register(ItemCategory, ItemCategoryAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(StoreOwner, StoreOwnerAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(MyBag, MyBagAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
